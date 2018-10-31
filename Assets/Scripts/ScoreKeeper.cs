ab0f328fb31341565f022d39e5ed26dcadefcbc8
vrdcuci <38484150+vrdcuci@users.noreply.github.com>
2018-10-30 18:44:58 -0700

    first commit

---
+++ Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreKeeper : MonoBehaviour {
+
+    public int score = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void updateScore()
+    {
+        score++;
+        this.gameObject.GetComponent<TextMeshProUGUI>().text = "Score: " + score;
+    }
+}
